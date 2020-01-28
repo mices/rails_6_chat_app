7317b58008b6d8a8f68206454a3301448c56dda0
Fugee Ohu <fugee279@gmail.com>
2020-01-28 15:18:07 -0500

    6th commit

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -23,12 +23,20 @@ consumer.subscriptions.create("RoomChannel", {
 			   if (data.sender==Cookies.get('current_user_id')) 
 				   {
 				   console.log ('sender matches current_user_id')
-					$('#message_holder').append('<div class="container-right"><div class="subscript">' + data.sender_name + '<br>' + data.msg_time + '</div>' + data.content.body + '<br>' + '<img src="' + data.attachment + '"></div>')
-				   }
+					$('#message_holder').append('<div class="container-right"><div class="subscript">' + data.sender_name + '<br>' + data.msg_time + '</div>' + data.content.body + '</div>')
+				   if (typeof data.attachment != "undefined") 
+					   {
+						  $('#message_holder').append('<img src="' + data.attachment + '">')
+					   }
 			   else
 				   {
 					console.log ('sender and current_user_id are different')
-					$('#message_holder').append('<div class="container-left"><div class="subscript">' + data.sender_name + '<br>' + data.msg_time + '</div>' + data.content.body + '<br>' + '<img src="' + data.attachment + '"></div>')
+					$('#message_holder').append('<div class="container-left"><div class="subscript">' + data.sender_name + '<br>' + data.msg_time + '</div>' + data.content.body + '</div>')
+				   if (typeof data.attachment != "undefined") 
+					   {
+						  $('#message_holder').append('<%= image_tag(' + m.attachment + ', height: 200) %>')
+					   }
+
 				   }
 
 	   }
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,5 @@ class Message < ApplicationRecord
 
   include ::ActionView::Helpers::TextHelper
   has_rich_text :content
-    acts_as_taggable
 
 end
